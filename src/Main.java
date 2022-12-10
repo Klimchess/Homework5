433bfcbdf2b496d9bcafd570672e0785bff12d24
klimchess <vanyusha.1996@mail.ru>
2022-12-10 16:57:39 +0300

    сделал дз

--- src/Main.java
+++ src/Main.java
@@ -23,10 +23,22 @@ public class Main {
         // задача 4
         int deliveryDistance = 95;
         if (deliveryDistance <= 20) {System.out.println( "Потребуется дней: 1");}
-        if (deliveryDistance > 20 && deliveryDistance <=60) {System.out.println( "Потребуется дней 2");}
-        if (deliveryDistance > 60 && deliveryDistance <=100) {System.out.println("Потребуется дней 3");}
-        if (deliveryDistance > 100) {System.out.println("Доставки нет");}
+        else if (deliveryDistance > 20 && deliveryDistance <=60) {System.out.println( "Потребуется дней 2");}
+        else if (deliveryDistance > 60 && deliveryDistance <=100) {System.out.println("Потребуется дней 3");}
+        else if  (deliveryDistance > 100){System.out.println("Доставки нет");}
         // задача 5
+        int monthNumber = 12;
+        switch (monthNumber) {case 12: case 1: case 2: System.out.println( "Зима");
+            break;
+            case 3: case 4: case 5: System.out.println("Весна");
+            break;
+            case 6: case 7: case 8: System.out.println("Лето");
+            break;
+            case 9: case 10: case 11: System.out.println("Осень");
+            break;
+            default: System.out.println("Такого не существует");
+        }
+
 
     }
 }
